8bf01bbab852378f33c1c4835dce313cd3377943
Albrecht <albrechtgoecking@gmail.com>
2026-03-05 17:24:51 -0300

    #Fix: tentativa de corrigir o problema de conexão com mysql

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ def create_app(config_name='development'):
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True, host='0.0.0.0', port=8000)  # ✅ Mudei para 8000
+    app.run(debug=True, host='0.0.0.0', port=8000)
